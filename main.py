0e5614ca5c005cfcd8dd97a9301e3ea2ae4ab8ad
afwolfe <3718652+afwolfe@users.noreply.github.com>
2022-06-13 07:14:37 -0400

    Add black formatting and flake8 linting

--- main.py
+++ main.py
@@ -15,16 +15,55 @@ from ynab_api.model import save_transaction
 
 load_dotenv()
 
-argparser = argparse.ArgumentParser(description='Convert Apple Card screenshots to YNAB transactions.')
-argparser.add_argument("-f", action="store_true", help="Force overwriting of outputPath.")
+argparser = argparse.ArgumentParser(
+    description="Convert Apple Card screenshots \
+  to YNAB transactions."
+)
+argparser.add_argument(
+    "-f", action="store_true", help="Force overwriting of outputPath."
+)
 cardvision = argparser.add_argument_group("cardvision")
-cardvision.add_argument("--cardVisionPath", default="CardVision", help="The path to use for CardVision. Defaults to 'CardVision/'", required=False)
-cardvision.add_argument("--imagePath", default="screenshots/", help="The path to the folder containing the screenshots for CardVision. Defaults to 'screenshots'", required=False)
-cardvision.add_argument("--outputPath", default="transactions.csv", help="The file to write the transaction data from CardVision. Defaults to 'transactions.csv'", required=False)
+cardvision.add_argument(
+    "--cardVisionPath",
+    default="CardVision",
+    help="The path to use for CardVision. Defaults to 'CardVision/'",
+    required=False,
+)
+cardvision.add_argument(
+    "--imagePath",
+    default="screenshots/",
+    help="The path to the folder containing the screenshots for CardVision. \
+    Defaults to 'screenshots'",
+    required=False,
+)
+cardvision.add_argument(
+    "--outputPath",
+    default="transactions.csv",
+    help="The file to write the transaction data from CardVision. \
+    Defaults to 'transactions.csv'",
+    required=False,
+)
 ynab = argparser.add_argument_group("ynab")
-ynab.add_argument("--budgetId", default=os.environ.get("YNAB_BUDGET_ID"), help="The YNAB budget ID to write to, defaults to YNAB_BUDGET_ID in env.", required=False)
-ynab.add_argument("--apiKey", default=os.environ.get("YNAB_API_KEY"), help="The YNAB API key to use to, defaults to YNAB_API_KEY in env.", required=False)
-ynab.add_argument("--accountId", default=os.environ.get("YNAB_APPLE_CARD_ACCOUNT_ID"), help="The YNAB account ID of the Apple Card, defaults to YNAB_APPLE_CARD_ACCOUNT_ID in env. Will attempt to read find account with 'Apple' in budget if not specified.", required=False)
+ynab.add_argument(
+    "--budgetId",
+    default=os.environ.get("YNAB_BUDGET_ID"),
+    help="The YNAB budget ID to write to, defaults to YNAB_BUDGET_ID in env.",
+    required=False,
+)
+ynab.add_argument(
+    "--apiKey",
+    default=os.environ.get("YNAB_API_KEY"),
+    help="The YNAB API key to use to, defaults to YNAB_API_KEY in env.",
+    required=False,
+)
+ynab.add_argument(
+    "--accountId",
+    default=os.environ.get("YNAB_APPLE_CARD_ACCOUNT_ID"),
+    help="The YNAB account ID of the Apple Card, defaults to YNAB_APPLE_CARD_ACCOUNT_ID in env. \
+    Will attempt to read find account with 'Apple' in budget if not specified.",
+    required=False,
+)
+
 args = argparser.parse_args()
 
 force: bool = args.f
@@ -45,12 +84,11 @@ api_key: str = args.apiKey
 account_id: str = args.accountId
 
 # YNAB API configuration
-configuration = ynab_api.Configuration(
-    host="https://api.youneedabudget.com/v1")
-configuration.api_key['bearer'] = api_key
-configuration.api_key_prefix['bearer'] = 'Bearer'
+configuration = ynab_api.Configuration(host="https://api.youneedabudget.com/v1")
+configuration.api_key["bearer"] = api_key
+configuration.api_key_prefix["bearer"] = "Bearer"
 
-summary_keys = ['date', 'payee_name', 'amount', 'memo']
+summary_keys = ["date", "payee_name", "amount", "memo"]
 
 
 def call_CardVisionCli(input_folder: os.PathLike, output_file: os.PathLike) -> None:
@@ -69,9 +107,14 @@ def call_CardVisionCli(input_folder: os.PathLike, output_file: os.PathLike) -> N
         raise Exception("Error occurred while building the cardvision CLI")
 
     # Call CardVision
-    cardvision_command = ["swift", "run", "CardVisionCLI",
-        "-imagePath", input_folder,
-        "-outputPath", output_file
+    cardvision_command = [
+        "swift",
+        "run",
+        "CardVisionCLI",
+        "-imagePath",
+        input_folder,
+        "-outputPath",
+        output_file,
     ]
     try:
         subprocess.run(cardvision_command, cwd=card_vision_path, check=True)
@@ -104,11 +147,13 @@ def amount_to_milliunit(amount: int) -> int:
     return int(float(amount) * 1000)
 
 
-def parse_cardvision_csv(csv_file: os.PathLike) -> List[save_transaction.SaveTransaction]:
+def parse_cardvision_csv(
+    csv_file: os.PathLike,
+) -> List[save_transaction.SaveTransaction]:
     """Uses the given csv file to create a list of YNAB SaveTransactions"""
     transactions = []
     summaries = []
-    with open(csv_file, 'r') as file:
+    with open(csv_file, "r") as file:
         transaction_rows = csv.DictReader(file)
         for row in transaction_rows:
             if row["Declined"] != "true":  # don't import declined transactions
@@ -118,7 +163,7 @@ def parse_cardvision_csv(csv_file: os.PathLike) -> List[save_transaction.SaveTra
                     amount=amount_to_milliunit(row["Amount"]),
                     payee_name=row["Payee"][:50],
                     memo=row["Memo"][:200],
-                    cleared="uncleared" if row["Pending"] == "true" else "cleared"
+                    cleared="uncleared" if row["Pending"] == "true" else "cleared",
                 )
                 summary = [transaction[f] for f in summary_keys]
                 summaries.append(summary)
@@ -131,17 +176,19 @@ def send_transactions_to_ynab(transactions: List[save_transaction.SaveTransactio
     """Sends the POST request to create the transactions in YNAB"""
     with ynab_api.ApiClient(configuration) as api_client:
         api_instance = transactions_api.TransactionsApi(api_client)
-        api_response = api_instance.create_transaction(budget_id, {"transactions": transactions})
+        api_response = api_instance.create_transaction(
+            budget_id, {"transactions": transactions}
+        )
         pprint(api_response)
 
 
 if __name__ == "__main__":
     call_CardVisionCli(image_path, output_path)
     transactions = parse_cardvision_csv(output_path)
-    confirm = ''
-    while confirm.lower() not in ['y', 'n']:
+    confirm = ""
+    while confirm.lower() not in ["y", "n"]:
         confirm = input("Send transactions to YNAB? (y/n) ")
-    if confirm.lower() == 'y':
+    if confirm.lower() == "y":
         send_transactions_to_ynab(transactions)
     else:
         print("Not sending transactions.")
