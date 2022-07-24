ab02df82cbacaf9459c49e266667846bec683226
afwolfe <3718652+afwolfe@users.noreply.github.com>
2022-07-24 19:05:48 -0400

    cardvisionpy - Add additional test cases

--- cardvisionpy/tests/transactionparsertest.py
+++ cardvisionpy/tests/transactionparsertest.py
@@ -1,4 +1,5 @@
-from datetime import date, timedelta
+import dateparser
+
 from cardvisionpy.logic.transactionparser import TransactionParser
 
 def test_next_transaction():
@@ -16,7 +17,42 @@ def test_next_transaction():
     assert(actual.is_daily_cash())
     assert(not actual.is_declined())
     assert(not actual.is_pending())
-    assert(actual.date == date.today() - timedelta(days=1))
+    assert(actual.date == dateparser.parse('Yesterday').date())
+
+def test_transaction_amount_first():
+    raw_transaction = [
+        '$100.00', 'Grocery Store', 'Card Number Used', '1%', '5 hours ago'
+    ]
+    
+    trot = TransactionParser(raw_transaction)
+    actual = trot.next_transaction()
+
+    assert(actual.payee == "Grocery Store")
+    assert(actual.amount == -10000)
+    assert(actual.memo == "Card Number Used")
+    assert(actual.dailyCash == "1%")
+    assert(actual.is_daily_cash())
+    assert(not actual.is_declined())
+    assert(not actual.is_pending())
+    assert(actual.date == dateparser.parse('5 hours ago').date())
+
+
+def test_transaction_daily_cash_before_memo():
+    raw_transaction = [
+        'Movie Theater', '$25.00', '2%', 'Pending - Card Number Used', '5 minutes ago'
+    ]
+    
+    trot = TransactionParser(raw_transaction)
+    actual = trot.next_transaction()
+
+    assert(actual.payee == "Movie Theater")
+    assert(actual.amount == -2500)
+    assert(actual.memo == "Pending - Card Number Used")
+    assert(actual.dailyCash == "2%")
+    assert(actual.is_daily_cash())
+    assert(not actual.is_declined())
+    assert(actual.is_pending())
+    assert(actual.date == dateparser.parse('5 minutes ago').date())
 
 def test_daily_cash_adjustment():
     raw_transaction = ['Daily Cash Adjustment', '$0.25', 'From Refund', 'Yesterday']
@@ -30,4 +66,4 @@ def test_daily_cash_adjustment():
     assert(not actual.is_daily_cash())
     assert(not actual.is_declined())
     assert(not actual.is_pending())
-    assert(actual.date == date.today() - timedelta(days=1))
+    assert(actual.date ==  dateparser.parse('Yesterday').date())
