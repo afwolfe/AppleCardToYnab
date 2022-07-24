521e5d8b0a0ca36e15824d38ae2116897d4bfd89
afwolfe <3718652+afwolfe@users.noreply.github.com>
2022-07-24 16:39:38 -0400

    Directly convert cardvisionpy transactions to YNAB

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import argparse
-import csv
 import logging
 import os
 import sys
 from typing import List
 
-from dateutil import parser
 from dotenv import load_dotenv
-from pprint import pprint
 from tabulate import tabulate
 
 import ynab_api
@@ -15,15 +12,13 @@ from ynab_api.api import accounts_api, transactions_api
 from ynab_api.model import save_transaction
 
 from cardvisionpy import cardvisionpy
+from cardvisionpy.models.transaction import Transaction
 
 load_dotenv()
 
 argparser = argparse.ArgumentParser(
     description="Convert Apple Card screenshots \
   to YNAB transactions."
-)
-argparser.add_argument(
-    "-f", action="store_true", help="Force overwriting of outputPath."
 )
 argparser.add_argument(
     "-d", action="store_true", help="Enables debug printing."
@@ -34,13 +29,6 @@ cardvision.add_argument(
     default="screenshots/",
     help="The path to the folder containing the screenshots for CardVision. \
     Defaults to 'screenshots'",
-    required=False,
-)
-cardvision.add_argument(
-    "--outputPath",
-    default="transactions.csv",
-    help="The file to write the transaction data from CardVision. \
-    Defaults to 'transactions.csv'",
     required=False,
 )
 ynab = argparser.add_argument_group("ynab")
@@ -66,7 +54,6 @@ ynab.add_argument(
 
 args = argparser.parse_args()
 
-force: bool = args.f
 debug: bool = args.d
 
 logger = logging.getLogger()
@@ -76,19 +63,16 @@ if debug:
     logger.setLevel("DEBUG")
     handler.setLevel("DEBUG") 
 else:
-    # logger.setLevel("INFO")
-    handler.setLevel("DEBUG") 
+    logger.setLevel("INFO")
+    handler.setLevel("INFO") 
 log_format = logging.Formatter('[%(asctime)s] [%(levelname)s] - %(message)s')
 handler.setFormatter(log_format)
 logger.addHandler(handler)
 
 image_path: os.PathLike = os.path.abspath(args.imagePath)
-output_path: os.PathLike = os.path.abspath(args.outputPath)
 
 if not os.path.exists(image_path):
     raise FileNotFoundError(f"imagePath: {image_path} does not exist")
-if os.path.exists(output_path) and not force:
-    raise FileExistsError(f"outputPath: {output_path} already exists.")
 
 budget_id: str = args.budgetId
 api_key: str = args.apiKey
@@ -100,19 +84,6 @@ configuration.api_key["bearer"] = api_key
 configuration.api_key_prefix["bearer"] = "Bearer"
 
 summary_keys = ["date", "payee_name", "amount", "memo"]
-
-
-def call_cardvision(input_folder: os.PathLike, output_file: os.PathLike) -> None:
-    """Uses the cardvisionpy with the specified input and output paths."""
-    try:
-        # TODO: Parse Transactions directly instead of serializing to CSV first.
-        transactions = cardvisionpy.get_processed_transactions(input_folder)
-        cardvisionpy.write_to_csv(transactions, output_file)
-        if not os.path.exists(output_file):
-            raise FileNotFoundError("Output file was not detected")
-    except Exception:
-        raise Exception("Error occurred while processing screenshots")
-
 
 def get_apple_card_account_id() -> str:
     """If account_id is not already stored,
@@ -140,30 +111,24 @@ def cents_to_milliunit(amount: int) -> int:
     return int(float(amount) * 10)
 
 
-def parse_cardvision_csv(
-    csv_file: os.PathLike,
-) -> List[save_transaction.SaveTransaction]:
-    """Uses the given csv file to create a list of YNAB SaveTransactions"""
+def cardvision_to_ynab_transactions(cv_transactions: list[Transaction]) -> List[save_transaction.SaveTransaction]:
     transactions = []
     summaries = []
-    with open(csv_file, "r") as file:
-        transaction_rows = csv.DictReader(file)
-        for row in transaction_rows:
-            if row["declined"] != "true":  # don't import declined transactions
-                transaction = save_transaction.SaveTransaction(
-                    account_id=get_apple_card_account_id(),
-                    date=parser.parse(row["date"]).date(),
-                    amount=cents_to_milliunit(row["amount"]),
-                    payee_name=row["payee"][:50],
-                    memo=row["memo"][:200],
-                    cleared="uncleared" if row["pending"] == "True" else "cleared",
-                )
-                summary = [transaction[f] for f in summary_keys]
-                summaries.append(summary)
-                transactions.append(transaction)
+    for cv_transaction in cv_transactions:
+        if not cv_transaction.is_declined():  # don't import declined transactions
+            ynab_transaction = save_transaction.SaveTransaction(
+                account_id=get_apple_card_account_id(),
+                date=cv_transaction.date,
+                amount=cents_to_milliunit(cv_transaction.amount),
+                payee_name=cv_transaction.payee[:50],
+                memo=cv_transaction.memo[:200],
+                cleared="uncleared" if cv_transaction.is_pending() else "cleared",
+            )
+            summary = [ynab_transaction[f] for f in summary_keys]
+            summaries.append(summary)
+            transactions.append(ynab_transaction)
     print(tabulate(summaries, headers=summary_keys))
     return transactions
-
 
 def send_transactions_to_ynab(transactions: List[save_transaction.SaveTransaction]):
     """Sends the POST request to create the transactions in YNAB"""
@@ -176,8 +141,8 @@ def send_transactions_to_ynab(transactions: List[save_transaction.SaveTransactio
 
 
 if __name__ == "__main__":
-    call_cardvision(image_path, output_path)
-    transactions = parse_cardvision_csv(output_path)
+    cv_transactions = cardvisionpy.get_processed_transactions(image_path)
+    transactions = cardvision_to_ynab_transactions(cv_transactions)
     confirm = ""
     while confirm.lower() not in ["y", "n"]:
         confirm = input("Send transactions to YNAB? (y/n) ")
